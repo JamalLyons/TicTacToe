3ec569cce60382e7e5fe623b3391004fb4e5b0a4
CodingWithJamal <43446907+jamallyons@users.noreply.github.com>
2025-01-01 20:09:29 -0500

    first commit

---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by codingwithjamal on 1/1/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+// Standard includes
+#include <iostream>
+
+// External libraries
+#include "raylib.h"
+
+#endif //MAIN_H
